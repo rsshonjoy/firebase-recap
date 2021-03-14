02a4831da139a9f66b56760de346b4c0b82402ee
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-03-15 00:07:55 +0600

    firebase-racap

--- src/firebase.config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCFyy66u2jWoT0kEYfG_pYPJBoSlNv1tqQ",
-  authDomain: "auth-recap-bd686.firebaseapp.com",
-  projectId: "auth-recap-bd686",
-  storageBucket: "auth-recap-bd686.appspot.com",
-  messagingSenderId: "304295817007",
-  appId: "1:304295817007:web:e4384af7135adf56cc7bda"
-};
-export default firebaseConfig;
